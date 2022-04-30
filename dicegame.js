4e02f43f4faaef1f0653219100305f62060fd78d
unknown <samueloctavianus97@gmail.com>
2022-04-30 20:00:38 +0700

    wrote some pseudocode, updated some css

---
+++ dicegame.js
@@ -0,0 +1,7 @@
+//when the user hits the refresh button
+    //1. the first time the user enters the game, the header text will show as is
+    //2. Generate random numbers from 1 - 6
+    //3. assign that random number to two emptry seperate variable
+    //4. the first time the user hits the refresh button, the header will guide the user to 
+    //hit the refresh button.
+
